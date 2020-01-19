fedad51bd0db6599456cf9f24d48796ba4e4b4bb
BeeBeef <beefsoup6863@163.com>
2020-01-19 21:40:05 +0800

    2020-01-19 / 21:12 提交一下

---
+++ learn/spring-jsp-jdbc-mybatis/src/main/java/com/zhang/learn/LearnApplication.java
@@ -0,0 +1,16 @@
+package com.zhang.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 学习使用 模块化操作
+ */
+@SpringBootApplication
+public class LearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnApplication.class, args);
+	}
+
+}
