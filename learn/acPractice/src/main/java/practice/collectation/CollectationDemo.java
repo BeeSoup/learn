b89874b0bf49b3973a565050335254f60dd797f6
BeeBeef <beefsoup6863@163.com>
2020-01-19 21:12:22 +0800

    2020-01-19 / 21:12 提交一下

--- learn/acPractice/src/main/java/practice/collectation/CollectationDemo.java
+++ learn/acPractice/src/main/java/practice/collectation/CollectationDemo.java
@@ -10,6 +10,15 @@ package practice.collectation;
  * 知识点部分
  *
  *
+ * 第一阶段：
+ *    你知道了对方怎么实现
+ *
+ * 第二阶段：
+ *    你去运用它、用好它
+ *
+ * 第三阶段：
+ *    你怎么去创造它
+ *
  */
 public class CollectationDemo {
 }
