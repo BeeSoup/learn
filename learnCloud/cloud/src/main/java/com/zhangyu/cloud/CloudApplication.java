83d9909ebe07e2ed81632e817ff2882b1a59d8ea
BeeBeef <beefsoup6863@163.com>
2020-05-18 15:28:49 +0800

    2020/05/18 15:28 SpringCloud 测试提交

--- learnCloud/cloud/src/main/java/com/zhangyu/cloud/CloudApplication.java
+++ learnCloud/cloud/src/main/java/com/zhangyu/cloud/CloudApplication.java
@@ -3,6 +3,9 @@ package com.zhangyu.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * SpringCloud起步
+ */
 @SpringBootApplication
 public class CloudApplication {
 
