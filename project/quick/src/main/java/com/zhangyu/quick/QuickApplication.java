8cee0f332a3cdabfbf42e4fd82433f6934349393
BeeBeef <beefsoup6863@163.com>
2020-05-23 20:47:07 +0800

    2020-05-23 / 20:47 提交一下 秒杀工程创建

---
+++ project/quick/src/main/java/com/zhangyu/quick/QuickApplication.java
@@ -0,0 +1,16 @@
+package com.zhangyu.quick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ */
+@SpringBootApplication
+public class QuickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickApplication.class, args);
+	}
+
+}
