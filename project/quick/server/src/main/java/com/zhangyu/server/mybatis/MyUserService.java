84d13b169125c3c7964760cfd4f7b30653bfde23
BeeBeef <beefsoup6863@163.com>
2020-06-03 23:39:18 +0800

    2020-06-03 / UUID和分页

--- project/quick/server/src/main/java/com/zhangyu/server/mybatis/MyUserService.java
+++ project/quick/server/src/main/java/com/zhangyu/server/mybatis/MyUserService.java
@@ -1,13 +1,17 @@
 package com.zhangyu.server.mybatis;
 
 import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.zhangyu.server.dto.MyUserDto;
 import com.zhangyu.server.mapper.UserAnnotationMapper;
 import com.zhangyu.server.domain.MyUser;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import com.course.server.dto.PageDto;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
@@ -21,13 +25,24 @@ public class MyUserService {
     }
 
     /**
-     * 做一个转换
+     * 做一个转换 并且分页传出结果
      */
-    public MyUserDto selectUserDtoById(Long id) {
-        PageHelper.startPage(1, 1);//从第一页开始，找一条， 然后遇到的第一个Mapper 执行SQL插入limit分页
+    public void selectUserDtoById(Long id, PageDto<MyUserDto> pageDto) {
+        //会先做查询总条数，然后再分页
+        PageHelper.startPage(pageDto.getPage(), pageDto.getSize());//从第一页开始，找一条， 然后遇到的第一个Mapper 执行SQL插入limit分页
         MyUser myUser = userAnnotationMapper.selectUserById(id);
+
         MyUserDto myUserDto = new MyUserDto();
+
         BeanUtils.copyProperties(myUser, myUserDto);
-        return myUserDto;
+        //假设查出来是List
+        //假设这个是结果
+        List<MyUserDto> list = new ArrayList<>();
+        PageInfo<MyUserDto> info = new PageInfo<>(list);
+//        pageDto.setTotal(list.size()); OR
+       pageDto.setTotal(info.getTotal());
+       pageDto.setList(list);//处理后的结果
+
+//        return pageDto; 最终返回这个
     }
 }
