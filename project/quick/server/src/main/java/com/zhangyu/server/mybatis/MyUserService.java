84feff1249b21bc652ec8cdc12e07d3f03c1bdad
BeeBeef <beefsoup6863@163.com>
2020-06-03 23:20:00 +0800

    2020-06-03 / mybatis 分页

--- project/quick/server/src/main/java/com/zhangyu/server/mybatis/MyUserService.java
+++ project/quick/server/src/main/java/com/zhangyu/server/mybatis/MyUserService.java
@@ -1,7 +1,10 @@
 package com.zhangyu.server.mybatis;
 
+import com.github.pagehelper.PageHelper;
+import com.zhangyu.server.dto.MyUserDto;
 import com.zhangyu.server.mapper.UserAnnotationMapper;
 import com.zhangyu.server.domain.MyUser;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -15,5 +18,16 @@ public class MyUserService {
 
     public MyUser selectUserById(Long id) {
         return userAnnotationMapper.selectUserById(id);
+    }
+
+    /**
+     * 做一个转换
+     */
+    public MyUserDto selectUserDtoById(Long id) {
+        PageHelper.startPage(1, 1);//从第一页开始，找一条， 然后遇到的第一个Mapper 执行SQL插入limit分页
+        MyUser myUser = userAnnotationMapper.selectUserById(id);
+        MyUserDto myUserDto = new MyUserDto();
+        BeanUtils.copyProperties(myUser, myUserDto);
+        return myUserDto;
     }
 }
