59356688e9cd4b0b328090e86a2471d2d0f48a92
BeeBeef <beefsoup6863@163.com>
2020-06-04 15:53:23 +0800

    2020-06-04 / 大文件分片传输，再合并，或者可以基于阿里云的OSS，未实践。

--- project/quick/file/src/main/java/com/zhangyu/file/FileSystemBootstrap.java
+++ project/quick/file/src/main/java/com/zhangyu/file/FileSystemBootstrap.java
@@ -2,11 +2,8 @@ package com.zhangyu.file;
 
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
--- project/quick/file/src/main/java/com/zhangyu/file/controller/FileController.java
+++ project/quick/file/src/main/java/com/zhangyu/file/controller/FileController.java
@@ -13,6 +13,13 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * 大数据的文件，采用分片传输，然后再合并，输出对应的格式，
+ *      比如100M 分着100个1M的
+ *      本地保存文件的形式
+ *
+ *      阿里云OSS
+ */
 @RestController
 @Slf4j
 public class FileController {
@@ -34,6 +41,8 @@ public class FileController {
         String fileName = file.getOriginalFilename();
         log.info(String.format("-----name: %s", name));
         log.info(String.format("-----fileName: %s", fileName));
+        String suffix = fileName.substring(fileName.lastIndexOf(".") + 1);
+        log.info(String.format("-----suffix: %s", suffix));
         String uuid = UuidUtil.getUuid();
         log.info(String.format("-----uuid: %s", uuid));
 
@@ -52,7 +61,7 @@ public class FileController {
             pathChar = "\\SaveFile\\";
         }
         //保存uuid可以存数据库，获取解析找。
-        String fullPath = projectPath + pathChar + uuid + "-" + fileName;
+        String fullPath = projectPath + pathChar + uuid + "." + suffix;
         log.info(String.format("------fullPath: %s", fullPath));
 
         File saveFile = new File(fullPath);
@@ -65,4 +74,53 @@ public class FileController {
         }
         return result;
     }
+
+
+//    public void merge(FileDto fileDto) throws Exception {
+//        LOG.info("合并分片开始");
+//        String path = fileDto.getPath(); //http://127.0.0.1:9000/file/f/course\6sfSqfOwzmik4A4icMYuUe.mp4
+//        path = path.replace(FILE_DOMAIN, ""); //course\6sfSqfOwzmik4A4icMYuUe.mp4
+//        Integer shardTotal = fileDto.getShardTotal();
+//        File newFile = new File(FILE_PATH + path);
+//        FileOutputStream outputStream = new FileOutputStream(newFile, true);//文件追加写入
+//        FileInputStream fileInputStream = null;//分片文件
+//        byte[] byt = new byte[10 * 1024 * 1024];
+//        int len;
+//
+//        try {
+//            for (int i = 0; i < shardTotal; i++) {
+//                // 读取第i个分片
+//                fileInputStream = new FileInputStream(new File(FILE_PATH + path + "." + (i + 1))); //  course\6sfSqfOwzmik4A4icMYuUe.mp4.1
+//                while ((len = fileInputStream.read(byt)) != -1) {
+//                    outputStream.write(byt, 0, len);
+//                }
+//            }
+//        } catch (IOException e) {
+//            LOG.error("分片合并异常", e);
+//        } finally {
+//            try {
+//                if (fileInputStream != null) {
+//                    fileInputStream.close();
+//                }
+//                outputStream.close();
+//                LOG.info("IO流关闭");
+//            } catch (Exception e) {
+//                LOG.error("IO流关闭", e);
+//            }
+//        }
+//        LOG.info("合并分片结束");
+//
+//        System.gc();
+//        Thread.sleep(100);
+//
+//        // 删除分片
+//        LOG.info("删除分片开始");
+//        for (int i = 0; i < shardTotal; i++) {
+//            String filePath = FILE_PATH + path + "." + (i + 1);
+//            File file = new File(filePath);
+//            boolean result = file.delete();
+//            LOG.info("删除{}，{}", filePath, result ? "成功" : "失败");
+//        }
+//        LOG.info("删除分片结束");
+//    }
 }
