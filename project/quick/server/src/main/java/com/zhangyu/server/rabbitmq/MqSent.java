1e2e896c4e1bfd643988dca0411a5b6f40a0478d
BeeBeef <beefsoup6863@163.com>
2020-06-04 17:54:10 +0800

    2020-06-04 / MQ移动并整合上去

--- project/quick/server/src/main/java/com/zhangyu/server/rabbitmq/MqSent.java
+++ project/quick/server/src/main/java/com/zhangyu/server/rabbitmq/MqSent.java
@@ -1,54 +1,54 @@
-    package com.zhangyu.server.rabbitmq;
+package com.zhangyu.server.rabbitmq;
 
 
-    import com.zhangyu.server.config.rabbitmq.RabbitMqConfig;
-    import org.springframework.amqp.core.AmqpTemplate;
-    import org.springframework.beans.factory.annotation.Autowired;
-    import org.springframework.stereotype.Component;
+import com.zhangyu.server.config.rabbitmq.RabbitMqConfig;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-    import java.time.LocalDateTime;
+import java.time.LocalDateTime;
 
-    @Component
-    public class MqSent {
+@Component
+public class MqSent {
 
 
-            private AmqpTemplate amqpTemplate;
+    private AmqpTemplate amqpTemplate;
 
-            @Autowired
-            public void setAmqpTemplate(AmqpTemplate amqpTemplate) {
-                this.amqpTemplate = amqpTemplate;
-            }
+    @Autowired
+    public void setAmqpTemplate(AmqpTemplate amqpTemplate) {
+        this.amqpTemplate = amqpTemplate;
+    }
 
-            public void sent(String msg) {
-                String context = "sent--" + LocalDateTime.now() + " -- " + msg;
-                System.out.printf(" [test] sent: %s \n", context);
-                amqpTemplate.convertAndSend(RabbitMqConfig.RABBITMQ_NAME, context);
-            }
+    public void sent(String msg) {
+        String context = "sent--" + LocalDateTime.now() + " -- " + msg;
+        System.out.printf(" [test] sent: %s \n", context);
+        amqpTemplate.convertAndSend(RabbitMqConfig.RABBITMQ_NAME, context);
+    }
 
-            public void topicSent(String msg) {
+    public void topicSent(String msg) {
 
 
         //        CorrelationData correlationData = new CorrelationData();
         //        correlationData.setId(order.getMessageId()); //消息唯一id
 
-                String context = LocalDateTime.now() + " -- " + msg;
-                String routingKey = "topic.key1";
-                String routingKey2 = "topic.googleEmail.ko";
-                System.out.printf(" [%s]->(%s) sent: %s \n", RabbitMqConfig.TOPIC_EXCHANGE, routingKey, context);
-                amqpTemplate.convertAndSend(RabbitMqConfig.TOPIC_EXCHANGE, routingKey, context);
-                System.out.printf(" [%s]->(%s) sent: %s \n", RabbitMqConfig.TOPIC_EXCHANGE, routingKey2, context);
-                amqpTemplate.convertAndSend(RabbitMqConfig.TOPIC_EXCHANGE, routingKey2, context + "--toc2");
-            }
+        String context = LocalDateTime.now() + " -- " + msg;
+        String routingKey = "topic.key1";
+        String routingKey2 = "topic.googleEmail.ko";
+        System.out.printf(" [%s]->(%s) sent: %s \n", RabbitMqConfig.TOPIC_EXCHANGE, routingKey, context);
+        amqpTemplate.convertAndSend(RabbitMqConfig.TOPIC_EXCHANGE, routingKey, context);
+        System.out.printf(" [%s]->(%s) sent: %s \n", RabbitMqConfig.TOPIC_EXCHANGE, routingKey2, context);
+        amqpTemplate.convertAndSend(RabbitMqConfig.TOPIC_EXCHANGE, routingKey2, context + "--toc2");
+    }
 
-            public void fanoutSent(String msg) {
+    public void fanoutSent(String msg) {
 
-                String context = LocalDateTime.now() + " -- " + msg;
+        String context = LocalDateTime.now() + " -- " + msg;
 
-                System.out.printf(" [%s] sent: %s \n", RabbitMqConfig.FANOUT_EXCHANGE, context);
+        System.out.printf(" [%s] sent: %s \n", RabbitMqConfig.FANOUT_EXCHANGE, context);
 
-                amqpTemplate.convertAndSend(RabbitMqConfig.FANOUT_EXCHANGE, "", context);
-            }
+        amqpTemplate.convertAndSend(RabbitMqConfig.FANOUT_EXCHANGE, "", context);
+    }
 
-            // [topic_exchange]->(topic.key1) sent: 2020-04-29T15:41:35.075 -- 开始测试topic
-            // [topic_exchange]->(topic.googleEmail.ko) sent: 2020-04-29T15:41:35.075 -- 开始测试topic
-        }
+    // [topic_exchange]->(topic.key1) sent: 2020-04-29T15:41:35.075 -- 开始测试topic
+    // [topic_exchange]->(topic.googleEmail.ko) sent: 2020-04-29T15:41:35.075 -- 开始测试topic
+}
--- project/quick/server/src/main/java/com/zhangyu/server/rabbitmq/MyReceivers.java
+++ project/quick/server/src/main/java/com/zhangyu/server/rabbitmq/MyReceivers.java
@@ -1,88 +1,88 @@
-    package com.zhangyu.server.rabbitmq;
+package com.zhangyu.server.rabbitmq;
 
 
-    import com.zhangyu.server.config.rabbitmq.RabbitMqConfig;
-    import lombok.extern.slf4j.Slf4j;
-    import org.springframework.amqp.rabbit.annotation.RabbitHandler;
-    import org.springframework.amqp.rabbit.annotation.RabbitListener;
-    import org.springframework.stereotype.Component;
+import com.zhangyu.server.config.rabbitmq.RabbitMqConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
 
-    @Component
-    @Slf4j
-    public class MyReceivers {
+@Component
+@Slf4j
+public class MyReceivers {
 
-            // [test] sent: sent--2020-04-29T15:04:58.761 -- 开始测试MQ
-            //[text] --MyReceivers : sent--2020-05-29T15:04:58.761 -- 开始测试MQ
-            // [test] sent: sent--2020-04-29T15:04:58.938 -- 开始测试MQ
-            //[text] --MyReceivers2 : sent--2020-05-29T15:04:58.938 -- 开始测试MQ
-            // [test] sent: sent--2020-04-29T15:04:59.299 -- 开始测试MQ
-            //[text] --MyReceivers : sent--2020-05-29T15:04:59.299 -- 开始测试MQ
-            // [test] sent: sent--2020-04-29T15:04:59.679 -- 开始测试MQ
-            //[text] --MyReceivers2 : sent--2020-05-29T15:04:59.679 -- 开始测试MQ
-            // [test] sent: sent--2020-04-29T15:05:00.063 -- 开始测试MQ
-            //[text] --MyReceivers : sent--2020-05-29T15:05:00.063 -- 开始测试MQ
-            // [test] sent: sent--2020-04-29T15:05:00.479 -- 开始测试MQ
-            //[text] --MyReceivers2 : sent--2020-05-29T15:05:00.479 -- 开始测试MQ
+    // [test] sent: sent--2020-04-29T15:04:58.761 -- 开始测试MQ
+    //[text] --MyReceivers : sent--2020-05-29T15:04:58.761 -- 开始测试MQ
+    // [test] sent: sent--2020-04-29T15:04:58.938 -- 开始测试MQ
+    //[text] --MyReceivers2 : sent--2020-05-29T15:04:58.938 -- 开始测试MQ
+    // [test] sent: sent--2020-04-29T15:04:59.299 -- 开始测试MQ
+    //[text] --MyReceivers : sent--2020-05-29T15:04:59.299 -- 开始测试MQ
+    // [test] sent: sent--2020-04-29T15:04:59.679 -- 开始测试MQ
+    //[text] --MyReceivers2 : sent--2020-05-29T15:04:59.679 -- 开始测试MQ
+    // [test] sent: sent--2020-04-29T15:05:00.063 -- 开始测试MQ
+    //[text] --MyReceivers : sent--2020-05-29T15:05:00.063 -- 开始测试MQ
+    // [test] sent: sent--2020-04-29T15:05:00.479 -- 开始测试MQ
+    //[text] --MyReceivers2 : sent--2020-05-29T15:05:00.479 -- 开始测试MQ
 
-        @RabbitListener(queues = {
-                RabbitMqConfig.RABBITMQ_NAME
-        })
-        public void receiver(String context) {
-            System.out.printf("[text] --MyReceivers : %s\n", context);
-        }
-
-
-        /**
-         * 多个监听，只会默认循环接收。
-         */
-        @RabbitListener(queues = {
-                RabbitMqConfig.RABBITMQ_NAME
-        })
-        public void receiver2(String context) {
-            System.out.printf("[text] --MyReceivers2 : %s\n", context);
-        }
-
-        /**
-         *
-         */
-        @RabbitListener(queues = {
-                RabbitMqConfig.TOPIC_QUEUE1
-        })
-        public void topicReceiver(String context) {
-            System.out.printf("[text] --topicReceiver : %s\n", context);
-        }
-
-        /**
-         * 路由器模式，都会被匹配到
-         */
-        @RabbitListener(queues = {
-                RabbitMqConfig.TOPIC_QUEUE2
-        })
-        public void topicReceiver2(String context) {
-            System.out.printf("[text] --topicReceiver2 : %s\n", context);
-        }
-            //[text] --topicReceiver : 2020-04-29T15:41:35.075 -- 开始测试topic
-            //[text] --topicReceiver2 : 2020-04-29T15:41:35.075 -- 开始测试topic
-            //[text] --topicReceiver2 : 2020-04-29T15:41:35.075 -- 开始测试topic--toc2
-
-        /**
-         * 广播模式，都会被匹配到
-         */
-        @RabbitListener(queues = {
-                RabbitMqConfig.FANOUT_QUEUE
-        })
-        public void fanoutReceiver(String context) {
-            System.out.printf("[text] --fanoutReceiver : %s\n", context);
-        }
-
-        /**
-         * 广播模式，都会被匹配到
-         */
-        @RabbitListener(queues = {
-                RabbitMqConfig.FANOUT_QUEUE2
-        })
-        public void fanoutReceiver2(String context) {
-            System.out.printf("[text] --fanoutReceiver2 : %s\n", context);
-        }
-
+    @RabbitListener(queues = {
+            RabbitMqConfig.RABBITMQ_NAME
+    })
+    public void receiver(String context) {
+        System.out.printf("[text] --MyReceivers : %s\n", context);
     }
+
+
+    /**
+     * 多个监听，只会默认循环接收。
+     */
+    @RabbitListener(queues = {
+            RabbitMqConfig.RABBITMQ_NAME
+    })
+    public void receiver2(String context) {
+        System.out.printf("[text] --MyReceivers2 : %s\n", context);
+    }
+
+    /**
+     *
+     */
+    @RabbitListener(queues = {
+            RabbitMqConfig.TOPIC_QUEUE1
+    })
+    public void topicReceiver(String context) {
+        System.out.printf("[text] --topicReceiver : %s\n", context);
+    }
+
+    /**
+     * 路由器模式，都会被匹配到
+     */
+    @RabbitListener(queues = {
+            RabbitMqConfig.TOPIC_QUEUE2
+    })
+    public void topicReceiver2(String context) {
+        System.out.printf("[text] --topicReceiver2 : %s\n", context);
+    }
+    //[text] --topicReceiver : 2020-04-29T15:41:35.075 -- 开始测试topic
+    //[text] --topicReceiver2 : 2020-04-29T15:41:35.075 -- 开始测试topic
+    //[text] --topicReceiver2 : 2020-04-29T15:41:35.075 -- 开始测试topic--toc2
+
+    /**
+     * 广播模式，都会被匹配到
+     */
+    @RabbitListener(queues = {
+            RabbitMqConfig.FANOUT_QUEUE
+    })
+    public void fanoutReceiver(String context) {
+        System.out.printf("[text] --fanoutReceiver : %s\n", context);
+    }
+
+    /**
+     * 广播模式，都会被匹配到
+     */
+    @RabbitListener(queues = {
+            RabbitMqConfig.FANOUT_QUEUE2
+    })
+    public void fanoutReceiver2(String context) {
+        System.out.printf("[text] --fanoutReceiver2 : %s\n", context);
+    }
+
+}
