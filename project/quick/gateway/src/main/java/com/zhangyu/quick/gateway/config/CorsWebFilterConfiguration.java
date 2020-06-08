928e889cab1e63b39df858dbfd49e8a44994b172
BeeBeef <beefsoup6863@163.com>
2020-06-08 16:28:04 +0800

    2020-06-08 / 移动配置

--- project/quick/gateway/src/main/java/com/zhangyu/quick/gateway/config/CorsWebFilterConfiguration.java
+++ project/quick/gateway/src/main/java/com/zhangyu/quick/gateway/config/CorsWebFilterConfiguration.java
@@ -7,6 +7,9 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.util.pattern.PathPatternParser;
 
+/**
+ * 跨域的配置
+ */
 @Configuration
 public class CorsWebFilterConfiguration {
 
@@ -25,6 +28,32 @@ public class CorsWebFilterConfiguration {
         source.registerCorsConfiguration("/**", config);
 
         return new CorsWebFilter(source);
+
+        //return (ServerWebExchange ctx, WebFilterChain chain) -> {
+        //			ServerHttpRequest request = ctx.getRequest();
+        //			if (!CorsUtils.isCorsRequest(request)) {
+        //				return chain.filter(ctx);
+        //			}
+        //
+        //			HttpHeaders requestHeaders = request.getHeaders();
+        //			ServerHttpResponse response = ctx.getResponse();
+        //			HttpMethod requestMethod = requestHeaders.getAccessControlRequestMethod();
+        //			HttpHeaders headers = response.getHeaders();
+        //			headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, requestHeaders.getOrigin());
+        //			headers.addAll(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, requestHeaders.getAccessControlRequestHeaders());
+        //			if (requestMethod != null) {
+        //				headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, requestMethod.name());
+        //			}
+        //			headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+        //			headers.add(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, "all");
+        //			headers.add(HttpHeaders.ACCESS_CONTROL_MAX_AGE, "3600");
+        //			if (request.getMethod() == HttpMethod.OPTIONS) {
+        //				response.setStatusCode(HttpStatus.OK);
+        //				return Mono.empty();
+        //			}
+        //			return chain.filter(ctx);
+        //————————————————
+
     }
 
 }
