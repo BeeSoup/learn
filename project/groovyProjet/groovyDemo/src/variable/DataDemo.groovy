4aa1cd608e58325c2048741dfa5f510583bf5d87
BeeSoup <beefsoup6863@163.com>
2020-07-09 08:36:09 +0800

    1.闭包进阶

---
+++ project/groovyProjet/groovyDemo/src/variable/DataDemo.groovy
@@ -0,0 +1,15 @@
+package variable
+
+def list = [1, 2, 3, 4] //ArrayList
+int[] array1 = [1, 2, 3, 4] //数组
+def array2 = [1, 2, 3, 4] as int[] //数组
+
+
+
+def lists =[-2,2,1,-5]
+Collections.sort(lists)// 排序 [-5, -2, 1, 2]
+println lists
+
+lists.sort() //排序
+// << 添加
+// - 直接减掉
