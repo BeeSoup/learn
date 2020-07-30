abd0e5c67a1d48b41a3a56237abcf7f03188805b
BeeSoup <beefsoup6863@163.com>
2020-07-30 20:04:00 +0800

    1、提交一下

--- learn/doc/springboot2.0/springboot2.0.md
+++ learn/doc/springboot2.0/springboot2.0.md
@@ -8,6 +8,9 @@
 ###外部化配置
 ### 组件自动装配
 ### springboot Starter 
-
+#### 组件自动装配
+   * 激活： @EnableAutoConfiguration 开启自动化
+   * 配置： /META-INF/spring.factories --配置了包括启动className 配置的启动类
+   
 
 ##与Web的联系
---
+++ learn/myjava8/src/main/java/lambda/demo/NumDemo.java
@@ -0,0 +1,28 @@
+/*
+ * 软件版权: 恒生电子股份有限公司
+ * 修改记录:
+ * 修改日期     修改人员  修改说明
+ * ========    =======  ============================================
+ * 2020/7/16  zhang  新增
+ * ========    =======  ============================================
+ */
+
+package lambda.demo;
+
+import org.springframework.cglib.core.Converter;
+
+/**
+ * 功能说明:
+ *
+ * @author zhang
+ * @Date 2020/07/16
+ */
+public class NumDemo {
+
+
+    public static void main(String[] args) {
+        int num = 1;
+   //     Converter<Integer, String> stringConverter = (from) -> String.valueOf(from + num);
+        num = 3;
+    }
+}
