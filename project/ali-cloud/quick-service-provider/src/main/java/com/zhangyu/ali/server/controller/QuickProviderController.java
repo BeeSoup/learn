68323016746f7f723db0d2720c42e5d9df00d7c9
BeeSoup <beefsoup6863@163.com>
2021-01-18 17:33:58 +0800

    基础

--- project/ali-cloud/quick-service-provider/src/main/java/com/zhangyu/ali/server/controller/QuickProviderController.java
+++ project/ali-cloud/quick-service-provider/src/main/java/com/zhangyu/ali/server/controller/QuickProviderController.java
@@ -1,7 +1,13 @@
 package com.zhangyu.ali.server.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.TypeReference;
 import com.zhangyu.ali.server.dto.User;
 import com.zhangyu.ali.server.entity.ResponseData;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @Slf4j
@@ -44,6 +52,50 @@ public class QuickProviderController {
         responseData.setData(user);
         log.info("被调用了");
         return responseData;
+    }
+
+    public static void main(String[] args) {
+//        String bz = "BFAM_BZ", zjldlx = "BFAM_ZJLDLX", zclx = "BFAM_ZCLX", fylx = "BFAM_FYLX_FZ" ;
+//        String fileIds = "12,23";
+//        List<String> collect = Arrays.asList(Optional.ofNullable(fileIds).orElse(" ").split(",")).stream().filter(StringUtils::isNotBlank).collect(Collectors.toList());
+//        List<String> strings = Optional.ofNullable(Arrays.asList(fileIds.split(","))).orElse(Collections.emptyList());
+//        System.out.println(StringUtils.join(Arrays.asList(bz, zjldlx, zclx, fylx), ","));
+        Object[] objs = {"admin"};
+        Param build = Param.builder()
+                .obj(objs[0])
+                .build();
+        Map<String, Object> map = JSONObject.parseObject(JSONObject.toJSONString(build));
+        log.info(String.valueOf(map));
+        Object obj = map.get("obj");
+        if (obj instanceof JSONObject) {
+            Map<String, Object> stringObjectMap = JSONObject.parseObject(obj.toString(), new TypeReference<Map<String, Object>>() {
+            });
+        }
+
+
+        RequestDTO requestDTO = new RequestDTO();
+        requestDTO.setParam(new User(12L, "hanmeimei"));
+        requestDTO.setOperatorCode("admin");
+        requestDTO.setIp("127.0.0.1");
+        Object[] objs2 = {requestDTO};
+        Param build2 = Param.builder()
+                .obj(objs2[0])
+                .build();
+        Map<String, Object> map2 = JSONObject.parseObject(JSONObject.toJSONString(build2));
+        Object obj2 = map2.get("obj");
+        if (obj2 instanceof JSONObject) {
+            Map<String, Object> stringObjectMap = JSONObject.parseObject(obj2.toString(), new TypeReference<Map<String, Object>>() {
+            });
+        }
+        log.info(String.valueOf(map2));
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    static class Param {
+        private Object obj;
     }
 
 }
